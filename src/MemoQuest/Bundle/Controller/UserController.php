2cfd609c0833899731404121f4e3e9c4ebc44c2a
bryanlvs <devill_b@etna-alternance.net>
2014-08-27 19:13:29 +0200

    no message

---
+++ src/MemoQuest/Bundle/Controller/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Nmpolo\RestBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Component\HttpFoundation\Request;
+use MemoQuest\Bundle\Entity\User;
+
+class OrganisationController extends FOSRestController implements ClassResourceInterface
+{
+    /**
+     * Collection get action
+     * @var Request $request
+     * @return array
+     *
+     * @Rest\View()
+     */
+    public function cgetAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('MemoQuestBundle:User')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Get action
+     * @var integer $id Id of the entity
+     * @return array
+     *
+     * @Rest\View()
+     */
+    public function getAction($id)
+    {
+        $entity = $this->getEntity($id);
+
+        return array(
+            'entity' => $entity,
+        );
+    }
+
+    /**
+     * Get entity instance
+     * @var integer $id Id of the entity
+     * @return User
+     */
+    protected function getEntity($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('MemoQuestBundle:User')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('User inexistant');
+        }
+
+        return $entity;
+    }
+}
