381c8a45a14e75dd61af448c195060f5c141c565
BRYAN DEVILLE <devill_b@etna-alternance.net>
2014-08-28 21:58:39 +0200

    f

---
+++ src/MemoQuest/Bundle/Form/ListeType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MemoQuest\Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ListeType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('theme')
+            ->add('category')
+            ->add('owner')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MemoQuest\Bundle\Entity\Liste'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'memoquest_bundle_liste';
+    }
+}
