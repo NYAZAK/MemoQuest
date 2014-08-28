4572257542772e4635c225be229f4a2fe7f424a9
BRYAN DEVILLE <devill_b@etna-alternance.net>
2014-08-28 21:11:59 +0200

    t

---
+++ src/MemoQuest/Bundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace MemoQuest\Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MemoQuest\Bundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'memoquest_bundle_user';
+    }
+}
