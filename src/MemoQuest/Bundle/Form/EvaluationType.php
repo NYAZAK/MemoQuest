0a505cc07085ef2aa29108399977bd76f4510939
BRYAN DEVILLE <devill_b@etna-alternance.net>
2014-08-29 11:22:21 +0200

    df

---
+++ src/MemoQuest/Bundle/Form/EvaluationType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MemoQuest\Bundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EvaluationType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('note')
+            ->add('commentaire')
+            ->add('liste')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MemoQuest\Bundle\Entity\Evaluation'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'memoquest_bundle_evaluation';
+    }
+}
