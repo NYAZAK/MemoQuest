5c88e4c21965bbc0a82122732386bde855363f27
bryanlvs <devill_b@etna-alternance.net>
2014-08-29 09:42:01 +0200

    no message

--- src/MemoQuest/Bundle/Controller/MotListeController.php
+++ src/MemoQuest/Bundle/Controller/MotListeController.php
@@ -77,7 +77,7 @@ class MotListeController extends FOSRestController implements ClassResourceInter
 
             return $this->redirectView(
                 $this->generateUrl(
-                    'get_user_liste_mot_liste',
+                    'get_user_liste_mot',
                     array(
                         'userId' => $entity->getListe()->getOwner()->getId(),
                         'listeId' => $entity->getListe()->getId(),
