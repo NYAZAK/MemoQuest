470bc17e3ad513fd7c67818d943d7f989611bc5e
bryanlvs <devill_b@etna-alternance.net>
2014-12-05 16:04:14 +0100

    commit

---
+++ src/MemoQuest/Bundle/Controller/QuizContentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MemoQuest\Bundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Component\HttpFoundation\Request;
+use MemoQuest\Bundle\Entity\Quiz;
+use MemoQuest\Bundle\Entity\QuizContent;
+use MemoQuest\Bundle\Form\UserType;
+
+class QuizController extends FOSRestController implements ClassResourceInterface
+{
+    /**
+     * Collection get action
+     * @var Request $request
+     * @return array
+     *
+     * @Rest\View()
+     */
+    public function cgetAction(Request $request, $quizId)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('MemoQuestBundle:Quiz')->findBy(
+        	array(
+                'quiz' => $quizId,
+            )
+        );
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+}
---
+++ src/MemoQuest/Bundle/Controller/QuizController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MemoQuest\Bundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Routing\ClassResourceInterface;
+use FOS\RestBundle\Util\Codes;
+use Symfony\Component\HttpFoundation\Request;
+use MemoQuest\Bundle\Entity\Quiz;
+use MemoQuest\Bundle\Form\UserType;
+
+class QuizController extends FOSRestController implements ClassResourceInterface
+{
+    /**
+     * Collection get action
+     * @var Request $request
+     * @return array
+     *
+     * @Rest\View()
+     */
+    public function cgetAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('MemoQuestBundle:Quiz')->findAll();
+
+        return array(
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Get action
+     * @var integer $id login of the entity
+     * @return array
+     *
+     * @Rest\View()
+     */
+    public function getAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+    	
+        $entity = $em->getRepository('MemoQuestBundle:Quiz')->findOneBy(
+            array(
+                'id' => $id,
+            )
+        );
+        
+        return array(
+            'entity' => $entity,
+        );
+    }
+}
